69d862a0eea0a6446b47338cef9341709f6bb600
Adam Haney <adamhaney@users.noreply.github.com>
2019-01-02 16:28:41 -0500

    Attempting to fix import issue

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/sfdc-pcg/terraform-provider-slack/slack"
+	"github.com/adamhaney/terraform-provider-slack/slack"
 )
 
 func main() {
